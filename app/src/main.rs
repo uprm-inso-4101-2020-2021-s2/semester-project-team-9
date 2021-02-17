4840952aa44f0bfe19c2413d8181bbecaea219ff
RichardVB76 <richard.vazquez2@upr.edu>
2021-02-17 07:25:59 -0400

    Changes -RVB

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,10 @@
 mod models;
+mod config;
 
 use crate::models::Status;
 use actix_web::{App,HttpServer, web, Responder};
 use std::io;
+use dotenv::dotenv;
 
 
 //To check if the server is UP -RVB
@@ -16,7 +18,12 @@ async fn status() -> impl Responder{
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
 
-    println!("Starting server at http://127.0.0.1:8080");
+
+    dotenv().ok();
+    let config = crate::config::Config::from_env().unwrap();
+
+
+    println!("Starting server at http://{}:{}", config.server.host, config.server.port);
 
 
     HttpServer::new(move || {
@@ -24,7 +31,8 @@ async fn main() -> io::Result<()> {
             .route("/", web::get().to(status))
             //add aditional routes here
     })
-    .bind("127.0.0.1:8080")?
+    //.bind("127.0.0.1:8080")?
+    .bind( format ("{}:{}", config.server.host, config.server.port))?
     .run()
     .await
 }
