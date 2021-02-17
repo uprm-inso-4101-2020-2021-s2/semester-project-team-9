3b7025d66114f47b0a471d3b5e462c9bd699991f
RichardVB76 <richard.vazquez2@upr.edu>
2021-02-17 06:08:23 -0400

    Added status to check if server is up -RVB

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,19 @@
-use actix_web::{App,Error,HttpResponse,HttpServer,web};
-use actix_web::http::{StatusCode};
+use actix_web::{App,HttpServer, web, Responder};
+use std::io;
 
 mod routes;
 
+//To check if the server is UP -RVB
+ayns fn status() -> impl Responder{
+    "{\"status\": \"UP\"}"
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+
+    println!("Starting server at http://192.168.0.102:8080");
+
+
     HttpServer::new(move || {
         App::new()
             .route("/", web::get().to(routes::hello_world))
