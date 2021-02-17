c792d8573aa76d1ba47ba1bfddc1cfff20fc0818
RichardVB76 <richard.vazquez2@upr.edu>
2021-02-17 09:23:22 -0400

    App framework
    - Creating my first service (status).
    - Created Status file.
    - added Status file in main.rs.
    
    App configuration
    
    - Added 2 dependencies to the project: dotenv and do config to the cargo dependencies.
    - Created config.rs file.
    - Added mod config to main.rs.
    - Added a method to get an instance of the configuration called.
    - Added .env file with the Server Host and Port declared.

--- app/src/main.rs
+++ app/src/main.rs
@@ -9,8 +9,6 @@ use dotenv::dotenv;
 
 //To check if the server is UP -RVB
 async fn status() -> impl Responder{
-   // "{\"status\": \"UP\"}"
-
    web::HttpResponse::Ok()
     .json(Status {status: "UP".to_string()})
 }
@@ -22,16 +20,14 @@ async fn main() -> io::Result<()> {
     dotenv().ok();
     let config = crate::config::Config::from_env().unwrap();
 
-
     println!("Starting server at http://{}:{}", config.server.host, config.server.port);
-
 
     HttpServer::new(move || {
         App::new()
             .route("/", web::get().to(status))
             //add aditional routes here
     })
-    //.bind("127.0.0.1:8080")?
+
     .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
     .await
