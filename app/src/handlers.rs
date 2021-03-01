7aa9b619a93991d614481f1d4544da759c28a5f1
FrancisPatron <Francis.patron1@upr.edu>
2021-03-01 02:26:26 -0400

    Added tables: user & subscription_service along with the structs. Also added query in handlers.rs to get the services

--- app/src/handlers.rs
+++ app/src/handlers.rs
@@ -1,9 +1,27 @@
-use actix_web::{web, Responder};
+use crate::postgres;
+use actix_web::{web, Responder, HttpResponse};
 use crate::models::Status;
+use deadpool_postgres::{Pool, Client};
 
 
 //To check if the server is UP -RVB
 pub async fn status() -> impl Responder{
     web::HttpResponse::Ok()
      .json(Status {status: "UP".to_string()})
+ }
+ 
+ pub async fn get_subscriptions(db_pool: web::Data<Pool>) -> impl Responder {
+
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("error connecting to the database");
+
+    let result = postgres::get_services(&client)
+        .await;
+
+    match result {
+        Ok(services) => HttpResponse::Ok().json(services),
+        Err(_) => HttpResponse::InternalServerError().into()
+    }
  }
