3c1a46d2f50b1fd3d57a8323f6520ec2181586bb
Victor Laureano <victor.laureano@upr.edu>
2021-03-02 00:37:49 -0400

    Add Structure Grids, and ANTD

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Row, Col, Menu } from 'antd';
+import 'antd/dist/antd.css'
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Select Subscription
+      </h1>
+      <br/><br/>
+      <Row gutter = {[32,16]}>
+        <Col span={6} offset = {1} style = {{backgroundColor: 'blue', color: 'white', height: "211px"}}>b</Col>
+        <Col span={6} offset={2} style = {{backgroundColor: 'blue', color: 'white'}}>b</Col>
+        <Col span={6} offset ={2} style = {{backgroundColor: 'blue', color: 'white'}}>b</Col>
+        
+        
+        
+      </Row>
+      <br/><br/>
+      <br/><br/>
+      <br/><br/>
+      <Row gutter = {[32,16]}>
+        <Col span={6} offset = {1} style = {{backgroundColor: 'blue', color: 'white', height: "211px"}}>b</Col>
+        <Col span={6} offset={2} style = {{backgroundColor: 'blue', color: 'white'}}>b</Col>
+        <Col span={6} offset ={2} style = {{backgroundColor: 'blue', color: 'white'}}>b</Col>
+        
+        
+        
+      </Row>
+
     </div>
   );
 }
