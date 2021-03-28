e8f161785ec3e449f25d22dff5dde0d874c4dcf4
VictorLaureano <victor.laureano>
2021-03-28 09:55:40 -0400

    Refactor(Select): Changed From columns to cards.

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,36 +1,33 @@
 import './App.css';
-import { Row, Col, Menu } from 'antd';
+import { Row, Col, Menu, Card } from 'antd';
 import 'antd/dist/antd.css'
 
-
+const cardGenerator = ({ color = "",  image = "", md = 8, value = 0}) => (
+  <Col xs={24} md={md} offset={value}>
+    <Card hoverable style={{backgroundColor: {color}}} cover={<img alt = "card" src = {image}/>}>
+      <Card.Meta style={{backgroundColor: {color}}}
+      />
+    </Card>
+  </Col>
+);
 
 function App() {
   return (
-    <div className="App">
+    <div className="Select">
       <h1>
         Select Subscription
       </h1>
       <br/><br/>
-      <Row  type = "flex" gutter = {[32,16]}>
-        <Col span={6} offset = {1} style = {{backgroundColor: '#FF0000', color: 'white', height: "211px"}}> <img src = "/images/Netflix.png" style = {{width: "325px" , height: "211px", position: "absolute", left:"0"}} /></Col>
-        <Col span={6} offset={2} style = {{backgroundColor: '#19E038', color: 'white'}}><img src = "/images/hulu.png" style = {{width: "325px" , height: "211px", position: "absolute", left:"0"}}/></Col>
-        <Col span={6} offset ={2} style = {{backgroundColor: '#19BCE0', color: 'white'}}><img src = "/images/spotify.png" style = {{width: "325px" , height: "211px", position: "absolute", left:"0"}}/></Col>
-        
-        
-        
-      </Row>
-      <br/><br/>
-      <br/><br/>
-      <br/><br/>
-      <Row gutter = {[32,16]}>
-        <Col span={6} offset = {1} style = {{backgroundColor: '#0D4FB2', color: 'white', height: "211px"}}><img src = "/images/primevideo.jpg" style = {{width: "325px" , height: "211px", position: "absolute", left:"0"}}/></Col>
-        <Col span={6} offset={2} style = {{backgroundColor: '#0DA8B2', color: 'white'}}><img src = "/images/HBO_Max.jpeg" style = {{width: "325px" , height: "211px", position: "absolute", left:"0"}}/></Col>
-        <Col span={6} offset ={2} style = {{backgroundColor: '#FA19FF', color: 'white'}}><img src = "/images/disneyplus.png" style = {{width: "325px" , height: "211px", position: "absolute", left:"0"}}/></Col>
-        
-        
-        
-      </Row>
+      <Row  gutter = {[32,32]}>
+        {cardGenerator({color: '#FF0000', image: "/images/Netflix.png"})}
+        {cardGenerator({color: '#19E038', image: "/images/hulu.png"})}
+        {cardGenerator({color: '#19BCE0', image: "/images/spotify.png"})}    
 
+        {cardGenerator({color: '#0D4FB2', image: "/images/primevideo.jpg"})}
+        {cardGenerator({color: '#0DA8B2', image: "/images/HBO_Max.jpeg"})}
+        {cardGenerator({color: '#FA19FF', image: "/images/disneyplus.png"})}         
+        
+        </Row>
     </div>
   );
 }
