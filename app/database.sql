942c047e3ed56aaa392c810d3061dbe122232826
RichardVB76 <richard.vazquez2@upr.edu>
2021-03-28 20:05:36 -0400

    Added Info to DB

--- app/database.sql
+++ app/database.sql
@@ -25,12 +25,13 @@ CREATE TABLE custom_unique_services(
     category VARCHAR(255),
     plans VARCHAR(1024)
 );
+
 INSERT INTO custom_unique_services (owner_id,service_name,service_url,category,plans) 
     VALUES(
         'Chegg',
         'Chegg',
         'https://www.chegg.com/',
-        'educational',
+        'education',
         '{"monthly":7.99, "yearly":79.99}'
     );
 INSERT INTO subscription_services (service_name,service_url,category,plans) 
@@ -104,8 +105,131 @@ INSERT INTO subscription_services (service_name,service_url,category,plans)
         'entertainment',
         '{"monthly":64.99}'
     );
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Chegg',
+        'https://www.chegg.com/',
+        'education',
+        '{"Chegg Study":14.95,"Chegg Study Pack": 19.95}'
+    );
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Coursera',
+        'https://www.coursera.org/',
+        'education',
+        '{"Course Minimum":30.00,"Course Maxmimum": 80.00}'
+    );
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Course Hero',
+        'https://www.coursehero.com/',
+        'education',
+        '{"Quarterly Plan":19.95,"Montlhy Plan": 39.99,"Annual Plan": 9.95}'
+    );
 
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Codecademy',
+        'https://www.codecademy.com/',
+        'education',
+        '{"Pro Plan": 19.99}'
+    );
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Udacity',
+        'https://www.udacity.com/',
+        'education',
+        '{"Course Monthly": 399.99}'
+    );    
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Playstation+',
+        'https://www.playstation.com/',
+        'gaming',
+        '{"1 Month": 9.99,"3 Months": 8.33,"12 Months": 5.00}'
+    ); 
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Xbox Live Gold',
+        'https://www.xbox.com/',
+        'gaming',
+        '{"1 Month": 9.99,"3 Months": 8.33, "6 Months": 6.67,"12 Months": 5.00}'
+    ); 
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'EA Play',
+        'https://www.ea.com/',
+        'gaming',
+        '{"1 Month": 4.99,"Full Year": 24.99}'
+    ); 
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'EA Play Pro',
+        'https://www.ea.com/',
+        'gaming',
+        '{"1 Month": 14.99,"Full Year": 99.99}'
+    ); 
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'eHarmony',
+        'https://www.eharmony.com/',
+        'dating',
+        '{"1 Month": 59.95,"3 Month": 23.95,"6 Month": 29.95,"12 Month": 19.95}'
+    ); 
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Tinder Gold',
+        'https://www.tinder.com/',
+        'dating',
+        '{"1 Month": 14.99,"6 Months": 8.83,"12 Months": 6.92}'
+    ); 
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Tinder Plus',
+        'https://www.tinder.com/',
+        'dating',
+        '{"1 Month": 4.99,"6 Months": 3.00,"12 Months": 2.33}'
+    ); 
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Match Standard',
+        'https://www.match.com/',
+        'dating',
+        '{"1 Month": 19.99,"6 Months": 17.99,"12 Months": 15.99}'
+    ); 
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Match Premium',
+        'https://www.match.com/',
+        'dating',
+        '{"3 Months": 23.99,"6 Months": 19.99,"12 Months": 16.49}'
+    );
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'The League Regular',
+        'https://www.theleague.com/',
+        'dating',
+        '{"1 Month": 199.99,"3 Months": 99.99,"6 Months": 67.00}'
+    ); 
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'The League Premium',
+        'https://www.theleague.com/',
+        'dating',
+        '{"1 Month": 399.00,"3 Months": 299.00}'
+    );
 
-
-
-
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Zoosk Premium',
+        'https://www.zoosk.com/',
+        'dating',
+        '{"1 Month": 29.95,"3 Months": 19.98,"6 Months": 12.49,"12 Months": 12.49}'
+    );
+INSERT INTO subscription_services (service_name,service_url,category,plans) 
+    VALUES(
+        'Elite Singles Premium',
+        'https://www.elitesingles.com/',
+        'dating',
+        '{"Classic(1 Month)": 29.95,"Light(3 Months)": 19.98,"Comfort(6 Months)": 12.49,"12 Months": 12.49}'
+    );
